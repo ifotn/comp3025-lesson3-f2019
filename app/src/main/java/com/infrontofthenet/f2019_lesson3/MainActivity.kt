8e82d88e62eeeed21948abd02d64a88fa2e93152
ifotn <rich@infrontofthenet.com>
2019-09-18 09:32:37 -0400

    starting layout

---
+++ app/src/main/java/com/infrontofthenet/f2019_lesson3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.infrontofthenet.f2019_lesson3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
